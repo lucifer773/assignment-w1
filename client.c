8a10b3bdcd8c566d811104112e8d3011135601dd
lucifer773 <59466690+lucifer773@users.noreply.github.com>
2020-01-29 21:06:36 +0530

    Add files via upload
    path mtu found

--- client.c
+++ client.c
@@ -8,7 +8,7 @@
 #include<sys/types.h>
 #include<unistd.h>
 #include<arpa/inet.h>
-
+#include<limits.h>
 #include<netinet/in.h> 
 
 int main(){
@@ -19,9 +19,9 @@ int main(){
     char server_msg[1000]={0};
 
     int recv_num=0;
-
-    int send_num=1500;
-    int conv_num=htonl(send_num);
+    int i;
+    int mtu=15;
+    //int conv_num=htonl(send_num);
 
 
 
@@ -40,9 +40,26 @@ int main(){
     //valread=read(sock,server_msg,1000);
     valread=read(sock,&recv_num,sizeof(recv_num));
     printf("Server's msg: %d",ntohl(recv_num));
+    int mtu1[2];
+    mtu1[0]=mtu;
+    mtu1[1]=ntohl(recv_num);
+    int pmtu=INT_MIN;
+    //for(i=0;i<2;i++){
+      //  printf("%d\t",mtu1[i]);
+    //}
+
+    if(mtu1[0]<mtu1[1]){
+        pmtu=mtu1[0];
+
+    }
+    else{
+        pmtu=mtu1[1];
+    }
+    printf("Path mtu is %d",pmtu);
+    
     //printf("Server's response: %s\n",server_msg);
    // send(sock,client_welcome,sizeof(client_welcome),0);
-    write(sock,&conv_num,sizeof(conv_num));
+   // write(sock,&conv_num,sizeof(conv_num));
 
   
     close(sock);
